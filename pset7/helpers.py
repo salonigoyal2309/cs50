64831f7473cc22a600ab236315a48cce1d4bd433
Saloni Goyal <48411357+salonigoyal2309@users.noreply.github.com>
2019-05-13 18:53:44 +0530

    Create helpers.py

---
+++ pset7/helpers.py
@@ -0,0 +1,34 @@
+from nltk.tokenize import sent_tokenize
+
+def lines(a, b):
+    """Return lines in both a and b"""
+
+    let_a = set(a.split("\n"))
+    let_b = set(b.split("\n"))
+    return let_a & let_b
+
+
+def sentences(a, b):
+    """Return sentences in both a and b"""
+
+    a_sentences = set(sent_tokenize(a))
+    b_sentences = set(sent_tokenize(b))
+
+    return a_sentences & b_sentences
+
+def substring_tokenize(str, n):
+    substrings = []
+
+    for i in range(len(str) - n + 1):
+        substrings.append(str[i:i + n])
+
+    return substrings
+
+
+def substrings(a, b, n):
+    """Return substrings of length n in both a and b"""
+
+    a_substrings = set(substring_tokenize(a, n))
+    b_substrings = set(substring_tokenize(b, n))
+
+    return a_substrings & b_substrings
