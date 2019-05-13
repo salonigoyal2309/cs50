7fc1df733c59df00e08f132bde6b94c2521e3aa6
Saloni Goyal <48411357+salonigoyal2309@users.noreply.github.com>
2019-05-13 18:59:38 +0530

    Update cash.py

--- pset6/cash.py
+++ pset6/cash.py
@@ -1,40 +1,26 @@
-from cs50 import get_int
-from math import floor
+from cs50 import get_float
+while True:
+    x = get_float('Change owed: ')
+    if isinstance(x,float) and x>0:
+        break
 
+x = x*100
+cnt = 0
+x = int(x)
+while x>=25:
+    x = x-25
+    cnt = cnt+1
 
-def main():
-    digit1 = 0
-    digit2 = 0
-    num_digits = 0
-    sum_of_double_odds = 0
-    sum_of_evens = 0
-    cc_number = get_int("Number: ")
+while x>=10:
+    x = x-10
+    cnt = cnt+1
 
-    while cc_number > 0:
-        digit2 = digit1
-        digit1 = cc_number % 10
+while x>=5:
+    x = x-5
+    cnt = cnt+1
 
-        if num_digits % 2 == 0:
-            sum_of_evens += digit1
-        else:
-            multiple = 2 * digit1
-            sum_of_double_odds += (multiple // 10) + (multiple % 10)
+while x>=1:
+    x = x-1
+    cnt = cnt+1
 
-        cc_number //= 10
-        num_digits += 1
-
-    is_valid = (sum_of_evens + sum_of_double_odds) % 10 == 0
-    first_two_digits = (digit1 * 10) + digit2
-
-    if digit1 == 4 and num_digits >= 13 and num_digits <= 16 and is_valid:
-        print("VISA\n")
-    elif first_two_digits >= 51 and first_two_digits <= 55 and num_digits == 16 and is_valid:
-        print("MASTERCARD\n")
-    elif (first_two_digits == 34 or first_two_digits == 37) and num_digits == 15 and is_valid:
-        print("AMEX\n")
-    else:
-        print("INVALID\n")
-
-
-if __name__ == "__main__":
-    main()
+print(cnt)
